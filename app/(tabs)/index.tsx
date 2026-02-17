7957ae9ec92288530fb14ca113c94ea74d82539d
IWAJU14 <develop4iwaju@gmail.com>
2026-02-17 15:16:03 +0100

    Premier commit

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Home() {
+  return (
+    <View style={{flex:1,justifyContent:'center',alignItems:'center',backgroundColor:'#333'}}>
+      <Text style={{color:'white'}}>Mon projet Expo fonctionne 🚀</Text>
+    </View>
+  );
+}
