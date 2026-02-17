56c99a96dce943661bc3e880a1bafc4762ad964d
IWAJU14 <develop4iwaju@gmail.com>
2026-02-17 18:10:57 +0100

    troisieme

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function Home() {
   return (
     <View style={{flex:1,justifyContent:'center',alignItems:'center',backgroundColor:'#333'}}>
-      <Text style={{color:'white'}}>Welcome People</Text>
+      <Text style={{color:'white'}}> People</Text>
     </View>
   );
 }
